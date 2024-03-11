d8c15c1c6be73ce6fe3db99ec96a1b1372afd5a1
Daniel Rojo Pérez <danielrojo@gmail.com>
2024-03-11 11:02:30 +0100

    beers component with order

--- src/app/components/beers/beers/beers.component.ts
+++ src/app/components/beers/beers/beers.component.ts
@@ -1,25 +1,77 @@
 import { Component, OnInit } from '@angular/core';
 import { BeersService } from '../../../services/beers.service';
 import { Beer } from '../../../models/beer';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import { map, of } from 'rxjs';
+
+enum OrderOption{
+  alphAsc,
+  alphDesc,
+  abvAsc,
+  abvDesc
+}
+
 
 @Component({
   selector: 'app-beers',
   standalone: true,
-  imports: [],
+  imports: [NgbDropdownModule],
   templateUrl: './beers.component.html',
   styleUrl: './beers.component.scss'
 })
 export class BeersComponent implements OnInit{
 
   beers: Beer[] = [];
+  showBeers: Beer[] = [];
+
+  orderOption = OrderOption;
 
   constructor(private service: BeersService) { }
   ngOnInit(): void {
     console.log('onInit: ');
     this.service.beers$.subscribe(beers => {
       this.beers = beers;
+      this.showBeers = beers;
     });
     this.service.getBeers();
   }
 
+  handleDropDownClick(option: OrderOption) {
+    const observer = {
+      next: (beers: Beer[]) => {
+        this.showBeers = beers;
+      }
+    }
+    switch(option) {
+      case OrderOption.alphAsc:
+        // this.showBeers = this.beers.sort((a, b) => a.name.localeCompare(b.name));
+        of(this.beers).pipe(
+          map(beers => beers.sort((a, b) => a.name.localeCompare(b.name))
+        )).subscribe(observer)
+        break;
+      case OrderOption.alphDesc:
+        // this.showBeers = this.beers.sort((a, b) => b.name.localeCompare(a.name));
+        of(this.beers).pipe(
+          map(beers => beers.sort((a, b) => b.name.localeCompare(a.name))
+        )).subscribe(observer)
+        break;
+      case OrderOption.abvAsc:
+        // his.showBeers = this.beers.sort((a, b) => a.abv - b.abv);
+        of(this.beers).pipe(
+          map(beers => beers.sort((a, b) => a.abv - b.abv)
+        )).subscribe(observer)
+        break;
+      case OrderOption.abvDesc:
+        // this.showBeers = this.beers.sort((a, b) => b.abv - a.abv);
+        of(this.beers).pipe(
+          map(beers => beers.sort((a, b) => b.abv - a.abv)
+        )).subscribe(observer)
+        break;
+      default:
+        this.showBeers = this.beers;
+    }
+  }
+
 }
+
+
