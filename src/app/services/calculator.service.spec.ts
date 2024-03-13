4e73c122f6a215717cd92805fe48d1831695d059
Daniel Rojo Pérez <danielrojo@gmail.com>
2024-03-13 13:01:06 +0100

    some test for calculator

--- src/app/services/calculator.service.spec.ts
+++ src/app/services/calculator.service.spec.ts
@@ -15,4 +15,37 @@ describe('CalculatorService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should add two numbers', () => {
+      
+    service.handleNumber(1);
+    service.handleSymbol('+');
+    service.handleNumber(2);
+    service.handleSymbol('=');
+    service.display$.subscribe((value) => {
+      expect(value).toBe('1+2=3');
+    });
+
+    });
+
+    it ('should divide by zero', () => {
+      service.handleNumber(1);
+      service.handleSymbol('/');
+      service.handleNumber(0);
+      service.handleSymbol('=');
+      service.display$.subscribe((value) => {
+        expect(value).toBe('1/0=Infinity');
+      });
+    });
+
+    it ('divide 0 by 0 with Nan as result', () => {
+      service.handleNumber(0);
+      service.handleSymbol('/');
+      service.handleNumber(0);
+      service.handleSymbol('=');
+      service.display$.subscribe((value) => {
+        expect(value).toBe('0/0=NaN');
+      });
+    });
+  
 });
